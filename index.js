36ec33804388c64d2266f6a01cbe09b147176776
wilfrieddev0 <118926620+KFLandry@users.noreply.github.com>
2024-05-21 00:04:17 +0200

    ...

--- index.js
+++ index.js
@@ -2,34 +2,36 @@ let toggleDE  =  document.querySelector("#btnDE")
 let toggleTG  =  document.querySelector("#btnTG")
 let toggleTS  =  document.querySelector("#btnTS")
 let popupDetails=document.querySelector("#popupDetails")
-// popupDetails.popover = 'auto'
+let btnClose = document.querySelector("#close")
 
-// toggleDE.popoverTargetElement = popupDetails
-// toggleTG.popoverTargetElement = popupDetails
-// toggleTS.popoverTargetElement = popupDetails
-
-// toggleDE.popoverTargetAction = "toggle"
-// toggleTG.popoverTargetAction = "toggle"
-// toggleTS.popoverTargetAction = "toggle"
+popupDetails.popover = "auto"
+toggleDE.popoverTargetAction = "toggle"
+toggleTG.popoverTargetAction = "toggle"
+toggleTS.popoverTargetAction = "toggle"
+btnClose.popoverTargetAction = "toggle"
+toggleDE.popoverTargetElement = popupDetails
+toggleTG.popoverTargetElement = popupDetails
+toggleTS.popoverTargetElement = popupDetails
+btnClose.popoverTargetElement= popupDetails 
 
 toggleDE.addEventListener("click", () => {
     popupDetails.classList.toggle("flex")
-    popupDetails.classList.toggle("hidden")
     document.querySelector("#imgDE").classList.toggle("hidden", false)
     document.querySelector("#imgTS").classList.toggle("hidden", true)
     document.querySelector("#imgTG").classList.toggle("hidden", true)
 })
 toggleTG.addEventListener("click", () => {
     popupDetails.classList.toggle("flex")
-    popupDetails.classList.toggle("hidden")
     document.querySelector("#imgTG").classList.toggle("hidden", false)
     document.querySelector("#imgDE").classList.toggle("hidden", true)
     document.querySelector("#imgTS").classList.toggle("hidden", true)
 })
 toggleTS.addEventListener("click", () => {
     popupDetails.classList.toggle("flex")
-    popupDetails.classList.toggle("hidden")
     document.querySelector("#imgTS").classList.toggle("hidden", false)
     document.querySelector("#imgDE").classList.toggle("hidden", true)
     document.querySelector("#imgTG").classList.toggle("hidden", true)
+})
+btnClose.addEventListener('click', () => {
+    popupDetails.classList.toggle("flex")
 })
